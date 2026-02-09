d8d11399bf5cd92d3124a3745a0e43c48173b1bf
Froggles <elmsterm@gmail.com>
2026-02-09 13:54:01 -0500

    display error when trying to delete activity

--- src/activities/ActivityList.jsx
+++ src/activities/ActivityList.jsx
@@ -1,23 +1,29 @@
+import { useState } from "react";
 import { deleteActivity } from "../api/activities";
 import { useAuth } from "../auth/AuthContext";
 
 export default function ActivityList({ activities, syncActivities }) {
+  const [error, setError] = useState(null);
   return (
-    <ul>
-      {activities.map((activity) => (
-        <li key={activity.id}>
-          {activity.name + " "}
-          <DeleteButton
-            activityId={activity.id}
-            syncActivities={syncActivities}
-          />
-        </li>
-      ))}
-    </ul>
+    <>
+      {error && <p role="alert">{error}</p>}
+      <ul>
+        {activities.map((activity) => (
+          <li key={activity.id}>
+            {activity.name + " "}
+            <DeleteButton
+              activityId={activity.id}
+              syncActivities={syncActivities}
+              setError={setError}
+            />
+          </li>
+        ))}
+      </ul>
+    </>
   );
 }
 
-function DeleteButton({ activityId, syncActivities }) {
+function DeleteButton({ activityId, syncActivities, setError }) {
   const { token } = useAuth();
   if (!token) return;
 
