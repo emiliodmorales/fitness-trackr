1e98e1c1288a82beec4cbace333fb3a77da682e2
Froggles <elmsterm@gmail.com>
2026-02-09 13:39:51 -0500

    create a delete button

--- src/activities/ActivityList.jsx
+++ src/activities/ActivityList.jsx
@@ -1,9 +1,23 @@
+import { deleteActivity } from "../api/activities";
+import { useAuth } from "../auth/AuthContext";
+
 export default function ActivityList({ activities }) {
   return (
     <ul>
       {activities.map((activity) => (
-        <li key={activity.id}>{activity.name}</li>
+        <li key={activity.id}>
+          {activity.name + " "}
+          <DeleteButton />
+        </li>
       ))}
     </ul>
+  );
+}
+
+function DeleteButton() {
+  const { token } = useAuth();
+  if (!token) return;
+  return (
+    <button onClick={() => deleteActivity(token, activity.id)}>delete</button>
   );
 }
