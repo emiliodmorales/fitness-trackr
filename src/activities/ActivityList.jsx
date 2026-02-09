edab8278c03bfba33afe5cca49de3fba720401b2
Froggles <elmsterm@gmail.com>
2026-02-09 13:53:30 -0500

    refresh activity list after deletion

--- src/activities/ActivityList.jsx
+++ src/activities/ActivityList.jsx
@@ -1,23 +1,40 @@
 import { deleteActivity } from "../api/activities";
 import { useAuth } from "../auth/AuthContext";
 
-export default function ActivityList({ activities }) {
+export default function ActivityList({ activities, syncActivities }) {
   return (
     <ul>
       {activities.map((activity) => (
         <li key={activity.id}>
           {activity.name + " "}
-          <DeleteButton activityId={activity.id} />
+          <DeleteButton
+            activityId={activity.id}
+            syncActivities={syncActivities}
+          />
         </li>
       ))}
     </ul>
   );
 }
 
-function DeleteButton({ activityId }) {
+function DeleteButton({ activityId, syncActivities }) {
   const { token } = useAuth();
   if (!token) return;
+
+  const tryDeleteActivity = async (formData) => {
+    setError(null);
+
+    try {
+      await deleteActivity(token, activityId);
+      syncActivities();
+    } catch (e) {
+      setError(e.message);
+    }
+  };
+
   return (
-    <button onClick={() => deleteActivity(token, activityId)}>delete</button>
+    <form action={tryDeleteActivity}>
+      <button>delete</button>
+    </form>
   );
 }
