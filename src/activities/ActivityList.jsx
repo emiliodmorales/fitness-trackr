ea41449329e254b1e822d7df662ca6f211524375
Froggles <elmsterm@gmail.com>
2026-02-10 12:49:52 -0500

    move delete errors to list items

--- src/activities/ActivityList.jsx
+++ src/activities/ActivityList.jsx
@@ -1,12 +1,8 @@
-import { useState } from "react";
-import { deleteActivity } from "../api/activities";
-import { useAuth } from "../auth/AuthContext";
+import { DeleteButton } from "./DeleteButton";
 
 export default function ActivityList({ activities, syncActivities }) {
-  const [error, setError] = useState(null);
   return (
     <>
-      {error && <p role="alert">{error}</p>}
       <ul>
         {activities.map((activity) => (
           <li key={activity.id}>
@@ -14,33 +10,10 @@ export default function ActivityList({ activities, syncActivities }) {
             <DeleteButton
               activityId={activity.id}
               syncActivities={syncActivities}
-              setError={setError}
             />
           </li>
         ))}
       </ul>
     </>
-  );
-}
-
-function DeleteButton({ activityId, syncActivities, setError }) {
-  const { token } = useAuth();
-  if (!token) return;
-
-  const tryDeleteActivity = async (formData) => {
-    setError(null);
-
-    try {
-      await deleteActivity(token, activityId);
-      syncActivities();
-    } catch (e) {
-      setError(e.message);
-    }
-  };
-
-  return (
-    <form action={tryDeleteActivity}>
-      <button>delete</button>
-    </form>
   );
 }
---
+++ src/activities/DeleteButton.jsx
@@ -0,0 +1,27 @@
+import { useState } from "react";
+import { deleteActivity } from "../api/activities";
+import { useAuth } from "../auth/AuthContext";
+
+export function DeleteButton({ activityId, syncActivities }) {
+  const [error, setError] = useState(null);
+  const { token } = useAuth();
+  if (!token) return;
+
+  const tryDeleteActivity = async (formData) => {
+    setError(null);
+
+    try {
+      await deleteActivity(token, activityId);
+      syncActivities();
+    } catch (e) {
+      setError(e.message);
+    }
+  };
+
+  return (
+    <form action={tryDeleteActivity}>
+      <button>delete</button>
+      {error && <p role="alert">{error}</p>}
+    </form>
+  );
+}
